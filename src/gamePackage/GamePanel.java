e159cd84bfd3d2d74bbddccc0610834506737fba
BuildTools <unconfigured@null.spigotmc.org>
2018-09-24 18:58:22 -0700

    Worked a little on the game but I am having issues with the keyPressed method. When enter is pressed once, keyPressed does not get called again. When it is called and is supposed to change the state, it only draws it if you adjust the size of the window.

--- src/gamePackage/GamePanel.java
+++ src/gamePackage/GamePanel.java
@@ -3,15 +3,20 @@ package gamePackage;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	Font menuFont;
+	Font instructionsFont;
 	final int MENU_STATE = 0;
 	final int FOREST_STATE = 1;
 	final int LAGOON_STATE = 2;
@@ -26,13 +31,67 @@ public class GamePanel extends JPanel {
 	// ocean2 and bay2 are after treasure is found
 	final int OCEAN2_STATE = 10;
 	final int BAY2_STATE = 11;
+	final int CREDITS_STATE = 12;
+	int currentState = MENU_STATE;
 
 	GamePanel() {
-		menuFont = new Font("Arial", Font.BOLD, 50);
+		menuFont = new Font("Arial", Font.ITALIC, 50);
+		instructionsFont = new Font("Arial", Font.PLAIN, 35);
+		//currentState = MENU_STATE;
 	}
 
-	public void paintComponent(Graphics g) {
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(0, 0, JourneyToTheLostTreasure.WIDTH, JourneyToTheLostTreasure.HEIGHT);
+		g.setColor(Color.gray);
+		g.setFont(menuFont);
+		g.drawString("Journey To The Lost Treasure", JourneyToTheLostTreasure.WIDTH / 7,
+				JourneyToTheLostTreasure.HEIGHT / 4);
+		g.setFont(instructionsFont);
+		g.drawString("Press ENTER to start", JourneyToTheLostTreasure.WIDTH/5,  JourneyToTheLostTreasure.HEIGHT/2);
+	}
+
+	void drawForestState(Graphics g) {
 		g.setColor(Color.GREEN);
 		g.fillRect(0, 0, JourneyToTheLostTreasure.WIDTH, JourneyToTheLostTreasure.HEIGHT);
 	}
+
+	public void paintComponent(Graphics g) {
+		if (currentState == MENU_STATE) {
+			drawMenuState(g);
+		} if (currentState == FOREST_STATE) {
+			drawForestState(g);
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (currentState == MENU_STATE) {
+			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+				System.out.println("ENTER was pressed");
+				currentState = FOREST_STATE;
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- src/gamePackage/JourneyToTheLostTreasure.java
+++ src/gamePackage/JourneyToTheLostTreasure.java
@@ -1,28 +1,35 @@
 package gamePackage;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class JourneyToTheLostTreasure {
 	static final int HEIGHT = 700;
 	static final int WIDTH = 1000;
 	JFrame f;
+	GamePanel gp;
 
 	public static void main(String[] args) {
-		JourneyToTheLostTreasure journey = new JourneyToTheLostTreasure();
-		journey.setup();
+		// JourneyToTheLostTreasure journey = new JourneyToTheLostTreasure();
+		new JourneyToTheLostTreasure();
+		// journey.setup();
 	}
 
 	JourneyToTheLostTreasure() {
 		f = new JFrame();
+		gp = new GamePanel();
+		setup();
 	}
 
 	void setup() {
-
-		f.setSize(WIDTH, HEIGHT);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
-		GamePanel gp = new GamePanel();
+		
 		f.add(gp);
+		f.addKeyListener(gp);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(WIDTH, HEIGHT);
+		
 
 	}
 }
